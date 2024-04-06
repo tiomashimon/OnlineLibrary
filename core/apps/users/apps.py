8d561b6c049763292404e0f8c6026f4d15156960
tiomashimon <tiomashimon@gmail.com>
2024-04-06 12:21:13 +0200

    feat: Added autocreation UserCharacteristics when user created

--- core/apps/users/apps.py
+++ core/apps/users/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.apps.users'
+
+    def ready(self):
+        import core.apps.users.signals
