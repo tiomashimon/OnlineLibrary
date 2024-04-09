5ce7b6bc0cfd3e0ab896e89a4f210c971f8f28eb
tiomashimon <tiomashimon@gmail.com>
2024-04-09 18:56:23 +0200

    feat:added task scheduling using celery-beat

--- core/project/celery.py
+++ core/project/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.project.settings.development')
 
@@ -8,3 +9,13 @@ app = Celery('main')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+
+
+app.conf.beat_schedule = {
+    'delete-expired-chats':
+    {
+        'task': 'core.apps.books.tasks.auto_assigning_fine',
+        'schedule': crontab(minute='*/5')
+    }
+}
