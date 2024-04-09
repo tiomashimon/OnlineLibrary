8d39d9f0bd1d0ad6a6fa9ba09e56651fa3b26159
tiomashimon <tiomashimon@gmail.com>
2024-04-09 09:13:51 +0200

    feat: Added user and user related endpoints

--- core/apps/users/views.py
+++ core/apps/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView
+from .serializers import (
+    UserSerializer, 
+    UserCharacteristicsSerializer,
+)
+from .models import (
+    User,
+    UserCharacteristics,
+)
 
-# Create your views here.
+class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+
+class UserCharacteristicsRetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    serializer_class = UserCharacteristicsSerializer
+    queryset = UserCharacteristics.objects.all()
+
+
+
